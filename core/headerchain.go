6e88961a9c85beb55a5f3ffa71e130cc59b90c5a
csquan <314588595@qq.com>
2022-11-17 21:11:45 +0800

    fix bug of sync

--- core/headerchain.go
+++ core/headerchain.go
@@ -516,14 +516,14 @@ func (hc *HeaderChain) GetHeadersFrom(number, count uint64) []rlp.RawValue {
 	}
 	var headers []rlp.RawValue
 	// If we have some of the headers in cache already, use that before going to db.
-	hash := rawdb.ReadCanonicalHash(hc.chainDb, number)
-	if hash == (common.Hash{}) {
-		return nil
-	}
 	for count > 0 {
+		hash := rawdb.ReadCanonicalHash(hc.chainDb, number)
+		if hash == (common.Hash{}) {
+			return nil
+		}
 		header, ok := hc.headerCache.Get(hash)
 		if !ok {
-			break
+			header = hc.GetHeader(hash, number)
 		}
 		h := header.(*types.Header)
 		rlpData, _ := rlp.EncodeToBytes(h)

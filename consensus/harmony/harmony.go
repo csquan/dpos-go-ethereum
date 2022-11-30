8d9f9fb285df5b43f98d09c08b890ee39ee6a8ce
csquan <314588595@qq.com>
2022-11-30 20:22:50 +0800

    fix mistach of voteTx

--- consensus/harmony/harmony.go
+++ consensus/harmony/harmony.go
@@ -567,22 +567,22 @@ func (h *Harmony) ApplyVoteTx(tx *types.Transaction) error {
 	case types.CandidateTxType:
 		if err = h.Ctx().BecomeCandidate(from); err != nil {
 			log.Warn("become candidate", "err", err)
-			return err
+			//return err
 		}
 	case types.UnCandidateTxType:
 		if err = h.Ctx().KickOutCandidate(from); err != nil {
 			log.Warn("leave candidate", "err", err)
-			return err
+			//return err
 		}
 	case types.DelegateTxType:
 		if err = h.Ctx().Delegate(from, *tx.To()); err != nil {
 			log.Warn("delegating", "err", err)
-			return err
+			//return err
 		}
 	case types.UnDelegateTxType:
 		if err = h.Ctx().UnDelegate(from, *tx.To()); err != nil {
 			log.Warn("leave delegating", "err", err)
-			return err
+			//return err
 		}
 	}
 	return nil
